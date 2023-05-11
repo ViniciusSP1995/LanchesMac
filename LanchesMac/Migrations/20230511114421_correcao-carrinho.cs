a24749b10f9f66443d12e7cf5b2d2d0de01a5464
Vinicius <vds1995sp@gmail.com>
2023-05-11 20:54:57 -0300

    primeira fase de aprendizado de desenvolvimento do projeto lanches mac em asp net

---
+++ LanchesMac/Migrations/20230511114421_correcao-carrinho.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    /// <inheritdoc />
+    public partial class correcaocarrinho : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CarrinhoCompraitemId",
+                table: "CarrinhoCompraItens",
+                newName: "CarrinhoCompraItemId");
+
+            migrationBuilder.RenameColumn(
+                name: "CarrinhCompraId",
+                table: "CarrinhoCompraItens",
+                newName: "CarrinhoCompraId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CarrinhoCompraItemId",
+                table: "CarrinhoCompraItens",
+                newName: "CarrinhoCompraitemId");
+
+            migrationBuilder.RenameColumn(
+                name: "CarrinhoCompraId",
+                table: "CarrinhoCompraItens",
+                newName: "CarrinhCompraId");
+        }
+    }
+}
